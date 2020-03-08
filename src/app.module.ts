4c34665107acaf2a71a0b4a160d5d4c1d85a2090
rivierha <48413049+rivierha@users.noreply.github.com>
2020-03-09 00:04:11 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { OrdersModule } from './orders/orders.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CustomersModule, OrdersModule, MongooseModule.forRoot('mongodb://localhost/shopify', { useNewUrlParser: true, useUnifiedTopology: true }),],
+  imports: [CustomersModule, OrdersModule, MongooseModule.forRoot('mongodb://172.31.32.161:27017/shopify', { useNewUrlParser: true, useUnifiedTopology: true }),],
   controllers: [AppController],
   providers: [AppService],
 })
