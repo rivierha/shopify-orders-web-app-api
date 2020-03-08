8e55623114f21679912fa3ca56bb7f847f2960d5
rivierha <48413049+rivierha@users.noreply.github.com>
2020-03-09 00:17:52 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,25 @@
 import { Controller, Res, Post, Body, Req, Logger,HttpStatus, HttpException } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('api/v1/')
+@Controller('')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  
+  @Get('/')
+    async getAllCustomers() {
+        try {
+            return res.status(HttpStatus.OK);
+        } catch (err) {
+            Logger.error(err);
+            throw new HttpException(
+                {
+                    status: HttpStatus.BAD_REQUEST,
+                    error: err,
+                },
+                400,
+            );
+        }
+    }
 
   @Post('/webhook/subscriptions')
   async createOrderAndCustomer(@Body() body, @Req() req, @Res() res){
